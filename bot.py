b49e3acca2d99d0b9506c4d8d6d3f313e8951ae5
GoodmanDev <david.g.goodman@bath.edu>
2022-11-06 18:38:39 +0000

    Update to 20.0 api + async

--- bot.py
+++ bot.py
@@ -1,11 +1,8 @@
 from jproperties import Properties
 
-from telegram.ext.updater import Updater
-from telegram.update import Update
-from telegram.ext.callbackcontext import CallbackContext
-from telegram.ext.commandhandler import CommandHandler
-from telegram.ext.messagehandler import MessageHandler
-from telegram.ext.filters import Filters
+from telegram import Update
+from telegram.ext import Application, CommandHandler, ContextTypes, MessageHandler
+import telegram.ext.filters
 
 
 configs = Properties()
@@ -13,30 +10,27 @@ with open('app-config.properties', 'rb') as config_file:
     configs.load(config_file)
 
 
-updater = Updater(
-    configs.get('api_key').data, use_context=True)
+app = Application.builder().token(configs.get('api_key').data).build()
+
+async def start(update: Update, context: ContextTypes.DEFAULT_TYPE):
+    await update.message.reply_text("Yo!")
 
 
-def start(update: Update, context: CallbackContext):
-    update.message.reply_text("Yo!")
+async def help(update: Update, context: ContextTypes.DEFAULT_TYPE):
+    await update.message.reply_text("Some helpful text..")
 
 
-def help(update: Update, context: CallbackContext):
-    update.message.reply_text("Some helpful text..")
-
-
-def unknown_text(update: Update, context: CallbackContext):
-    update.message.reply_text(
+async def unknown_text(update: Update, context: ContextTypes.DEFAULT_TYPE):
+    await update.message.reply_text(
         "Sorry I can't recognize you , you said {}".format(update.message.text))
 
 
-def unknown(update: Update, context: CallbackContext):
-    update.message.reply_text(
+async def unknown(update: Update, context: ContextTypes.DEFAULT_TYPE):
+    await update.message.reply_text(
         "Sorry {} is not a valid command".format(update.message.text))
 
-
-updater.dispatcher.add_handler(CommandHandler('start', start))
-updater.dispatcher.add_handler(CommandHandler('help', help))
-updater.dispatcher.add_handler(MessageHandler(Filters.command, unknown))
-updater.dispatcher.add_handler(MessageHandler(Filters.text, unknown_text))
-updater.start_polling()
+app.add_handler(CommandHandler('start', start))
+app.add_handler(CommandHandler('help', help))
+app.add_handler(MessageHandler(telegram.ext.filters.COMMAND, unknown))
+app.add_handler(MessageHandler(telegram.ext.filters.TEXT, unknown_text))
+app.run_polling()
