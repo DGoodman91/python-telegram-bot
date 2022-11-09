fc3c22e8b01f4979e679bc525caa9164f9283ab6
GoodmanDev <david.g.goodman@bath.edu>
2022-11-09 15:15:19 +0000

    Add post init and shutdown functions for the app

--- src/app_loader.py
+++ src/app_loader.py
@@ -10,11 +10,20 @@ class AppLoader():
     A class responsible for initializing and running the application
     """
 
+    async def post_init(self, app: Application) -> None:
+        """ Post-initialization function, called before polling starts """
+        print("Bot init complete, starting polling...")
+
+    async def post_shutdown(self, app: Application) -> None:
+        """ Shutdown function, use to close e.g., db connections """
+        print("Bot shutting down. Beep boop bop....")
+
     def run(self) -> None:
 
         config = ConfigLoader().load_app_config()
 
-        app = Application.builder().token(config['api_key']).build()
+        app = Application.builder().post_init(self.post_init).post_shutdown(
+            self.post_shutdown).token(config['api_key']).build()
 
         # TODO without the assignment, the code behaves the same but with a problem -
         # the application has a race condition where sometimes it exits immediately
